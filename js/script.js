6f4aa3aaf48c2442752e5b015f78a9b46ad01179
Cong Tien Dung Nguyen <Dung.Nguyen@student.souepl.cz>
2024-09-06 11:28:20 +0200

    wsz

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+
+function Add(){
+    let input = document.getElementById("input");
+    let output = document.getElementById("output");
+    /*console.log(input);
+    document.getElementById("output").innerHTML = input.value;
+
+    document.getElementById("output").innerHTML = document.getElementById("input").value;*/
+
+    output.innerHTML = input.value;
+}
